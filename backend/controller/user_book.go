e9e8d66a04843d55d93b7aa520d3c2ba6618d7f5
Yuki Yamamoto <yuki.yamamoto0915@gmail.com>
2021-04-07 18:28:23 +0900

    user_bookの骨組み実装

--- backend/controller/user_book.go
+++ backend/controller/user_book.go
@@ -1,1 +1,20 @@
 package controller
+
+import "github.com/gin-gonic/gin"
+
+type UserBookHandler interface {
+	FetchUserBook() gin.HandlerFunc
+}
+
+type userBookHandler struct {
+}
+
+func NewUserBookHandler() UserBookHandler {
+	return &userBookHandler{}
+}
+
+func (h *userBookHandler) FetchUserBook() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
--- backend/model/user_book.go
+++ backend/model/user_book.go
@@ -1,1 +1,36 @@
 package model
+
+import "database/sql"
+
+type UserBookModel interface {
+	InsertUserBook(record *UserBook) error
+	SelectUserBooks()
+	DeleteUserBook()
+}
+
+type userBookModel struct {
+	DB *sql.DB
+}
+
+func NewUserBookModel(db *sql.DB) UserBookModel {
+	return &userBookModel{DB: db}
+}
+
+type UserBook struct {
+	ID       string
+	UserID   string
+	English  string
+	Japanese string
+}
+
+func (m *userBookModel) InsertUserBook(record *UserBook) error {
+	return nil
+}
+
+func (m *userBookModel) SelectUserBooks() {
+
+}
+
+func (m *userBookModel) DeleteUserBook() {
+
+}
