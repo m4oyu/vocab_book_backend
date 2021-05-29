57d2dffc80bcaf9a790f9544b5dc65d7eb02c448
Yuki Yamamoto <yuki.yamamoto0915@gmail.com>
2021-05-29 18:32:35 +0900

    cors for deploy address

--- backend/main.go
+++ backend/main.go
@@ -156,9 +156,9 @@ func main() {
 		// 許可したいアクセス元の一覧
 		AllowOrigins: []string{
 			"http://localhost:8080",
-			"http://35.239.221.204:80",
+			//"http://35.239.221.204:80",
 			//"http://35.239.221.204:8080",
-			//"http://35.239.221.204",
+			"http://35.239.221.204",
 		},
 		// preflight requestで許可した後の接続可能時間
 		// https://godoc.org/github.com/gin-contrib/cors#Config の中のコメントに詳細あり
